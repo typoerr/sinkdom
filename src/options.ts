970d729b27de54cb4b610c920df980b11390086d
cotttpan <cotttpan@gmail.com>
2018-01-12 10:55:12 +0900

    chore(*): clean

--- src/options.ts
+++
@@ -1,7 +0,0 @@
-import { Observable } from './observable'
-import { Hook } from './hook'
-
-export interface Options {
-    hook?: Hook[]
-    proxy?(observable: Observable<any>): Observable<any>
-}
