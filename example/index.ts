d3f6d8c1f5b7ebd7a494a7f974437403935a684f
cotttpan <cotttpan@gmail.com>
2018-01-12 13:57:52 +0900

    chore(*): update example

--- example/index.ts
+++ example/index.ts
@@ -1,34 +1,65 @@
 /* tslint:disable:no-console */
 import { Observable, Scheduler } from 'rxjs'
-import { mount, div, VNode } from '../src/index'
+import { mount, div, VNode, span } from '../src/index'
+import { makeHashGroup } from '@cotto/utils.ts'
 
-const idx$ = Observable.timer(0, 2000)
-    .take(5)
-    .shareReplay(1)
+interface Item {
+    id: number,
+    content: number | string
+}
+
+function model(intent: { step$: Observable<number> }) {
+    const item = (id: number) => ({ id, content: id })
+    const [even$, odd$] = intent.step$.partition(i => i % 2 === 0)
+    return Observable.merge(
+        even$.mapTo(() => [1, 2, 3].map(item)),
+        odd$.mapTo(() => [1, 2, 3, 4].map(item)),
+    )
+}
+
+function store<S>(patch$: Observable<(state: S) => S>, init: any) {
+    return patch$.scan((state: S, fn) => fn(state), init)
+        .distinctUntilChanged()
+        .shareReplay(1)
+}
 
 const hook = {
     remove: (el: HTMLElement, _vnode: VNode) => (done: Function) => {
         el.style.color = 'red'
-        setTimeout(done, 1000)
+        setTimeout(done, 300)
     },
 }
+function listItem({ item, hash$ }: { item: Item, hash$: Observable<{ [key: string]: Item }> }) {
+    const id = String(item.id)
+    const content$ = hash$.pluck(id, 'content').filter(Boolean)
+    return div({ key: id, hook }, [
+        span(id + ': '),
+        span(content$),
+    ])
+}
 
-function view(items: Observable<number[]>) {
+function view(items: Observable<Item[]>) {
+    const hash$ = items.map(x => makeHashGroup(x, 'id')).shareReplay(1)
+    const _listItem = (item: Item) => listItem({ item, hash$ })
+
     return div([
-        items.switchMap(x => Observable.from(x)
+        items.switchMap(list => Observable.from(list)
             .observeOn(Scheduler.async)
-            .map(String)
-            .map(item => div({ key: String(item), hook }, item))
+            .map(_listItem)
             .toArray(),
         ),
     ])
 }
 
-const tree = view(idx$.map(n => n % 2 === 0 ? [1, 2, 3] : [1, 2, 3, 4]))
+const step$ = Observable.timer(0, 1000).take(10).shareReplay(1)
+const reducer$ = model({ step$ })
+const state$ = store(reducer$, [])
+const tree = view(state$)
 
-mount(tree, document.body, {
-    proxy: (value: Observable<any>) => value
-        .distinctUntilChanged()
-        .observeOn(Scheduler.async)
-        .subscribeOn(Scheduler.async),
-})
+const proxy = (value: Observable<any>) => value
+    .distinctUntilChanged()
+    .observeOn(Scheduler.animationFrame)
+    .subscribeOn(Scheduler.animationFrame)
+
+mount(tree, document.body, { proxy })
+state$.subscribe(console.log)
