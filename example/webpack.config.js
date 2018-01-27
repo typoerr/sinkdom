39314e23f4f1877e0a8176e04c1756f4986eadab
cotttpan <cotttpan@gmail.com>
2018-01-27 11:46:18 +0900

    chore(*)

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -2,8 +2,14 @@ const path = require('path')
 const webpack = require('webpack')
 const NotifierPlugin = require('webpack-notifier')
 
+/*
+npm run example -- --env.file=[filename]
+*/
+
 module.exports = env => {
     const file = env && env.file && env.file.split('.')[0] || 'svg'
+    console.log('example: ', file + '.ts') // tslint:disable-line:no-console
+
     return {
         context: path.resolve(__dirname, '../src'),
         entry: `../example/${file}.ts`,
